c878539388a945a4125b974c88a078d358eb5ec5
Holger Mueller <9jholger@gmail.com>
2018-03-27 16:03:22 -0500

    random letter generator working

--- assets/psychic.js
+++ assets/psychic.js
@@ -4,9 +4,10 @@ const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    console.log(letters);
+    //console.log(letters);
 
-//let letter = 
+let letter = letters[Math.floor(Math.random() * letters.length)];
+    //console.log(letter);
 
 let wins = 0;
 
