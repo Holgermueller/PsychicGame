6bcf83840f1a6df28b0c3da6db367f4ad844f9bb
Holger Mueller <9jholger@gmail.com>
2018-03-27 15:58:24 -0500

    console log keypresses

--- assets/psychic.js
+++ assets/psychic.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    console.log(letters);
+
+//let letter = 
+
+let wins = 0;
+
+let losses = 0;
+
+let livesLeft = 9;
+
+let lettersGuesses = [];
+
+document.onkeyup = function(event) {
+
+    let playerGuesses = event.key;
+    console.log(playerGuesses);
+
+}
