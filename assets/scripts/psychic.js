a688392e281c539dafa34b4b9b515f8090f511ec
Holger Mueller <9jholger@gmail.com>
2019-09-17 14:41:03 -0500

    updated node packages formatted files

--- assets/scripts/psychic.js
+++ assets/scripts/psychic.js
@@ -1,61 +1,78 @@
-'use strict';
+"use strict";
 
-const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-//generate letter to be guesses
 let letter = letters[Math.floor(Math.random() * letters.length)];
 
-//variables for game play
 let wins = 0;
 let losses = 0;
 let livesLeft = 9;
 let lettersGuesses = [];
 
-//capture players keyups
 document.onkeyup = event => {
-    let playerGuesses = event.key;
-    let letter = letters[Math.floor(Math.random() * letters.length)];
-    document.querySelector("#theLetter").innerHTML = "?";
-    //code for winning conditions
-    if (playerGuesses === letter) {
-        wins = wins + 1;
-        document.querySelector("#wins").innerHTML = "Wins: " + wins;
-        livesLeft = 9;
-        reset();
-        lettersGuesses = [];
-        document.querySelector("#lettersGuessed").innerHTML = " ";
-        //code for when a player guesses wrong letter
-    } else if (playerGuesses !== letter) {
-        livesLeft = livesLeft - 1;
-        document.querySelector("#livesLeft").innerHTML = "Lives Left: " + livesLeft;
-        //append incorrect guesses
-        document.querySelector("#lettersGuessed").append("" + playerGuesses + ", ");
-        //code for losing conditions
-    } if (livesLeft === 0) {
-        losses = losses + 1;
-        document.querySelector("#losses").innerHTML = "Losses: " + losses;
-        livesLeft = 9;
-        document.querySelector("#livesLeft").innerHTML = "Lives Left: " + livesLeft;
-        //reveal letter
-        document.querySelector("#theLetter").innerHTML = letter;
-        lettersGuesses = [];
-        document.querySelector("#lettersGuessed").innerHTML = " ";
-    }
-}
-
-//player guesses correct letter, letter to guess resets
-function reset() {
-    letter = letters[Math.floor(Math.random() * letters.length)];
-}
-
-document.onclick = function startOver() {
-    reset()
+  let playerGuesses = event.key;
+  let letter = letters[Math.floor(Math.random() * letters.length)];
+  document.querySelector("#theLetter").innerHTML = "?";
+  if (playerGuesses === letter) {
+    wins = wins + 1;
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+    livesLeft = 9;
+    reset();
+    lettersGuesses = [];
+    document.querySelector("#lettersGuessed").innerHTML = " ";
+  } else if (playerGuesses !== letter) {
+    livesLeft = livesLeft - 1;
+    document.querySelector("#livesLeft").innerHTML = "Lives Left: " + livesLeft;
+    document.querySelector("#lettersGuessed").append("" + playerGuesses + ", ");
+  }
+  if (livesLeft === 0) {
     losses = losses + 1;
     document.querySelector("#losses").innerHTML = "Losses: " + losses;
     livesLeft = 9;
     document.querySelector("#livesLeft").innerHTML = "Lives Left: " + livesLeft;
+    document.querySelector("#theLetter").innerHTML = letter;
     lettersGuesses = [];
     document.querySelector("#lettersGuessed").innerHTML = " ";
+  }
+};
+
+function reset() {
+  letter = letters[Math.floor(Math.random() * letters.length)];
 }
+
+document.onclick = function startOver() {
+  reset();
+  losses = losses + 1;
+  document.querySelector("#losses").innerHTML = "Losses: " + losses;
+  livesLeft = 9;
+  document.querySelector("#livesLeft").innerHTML = "Lives Left: " + livesLeft;
+  lettersGuesses = [];
+  document.querySelector("#lettersGuessed").innerHTML = " ";
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,33 @@
-const gulp = require('gulp');
-const less = require('gulp-less');
-const bs = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const less = require("gulp-less");
+const bs = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
 
-// set up browser-sync
-gulp.task('browser-sync', () => {
-    bs.init({
-        server: {
-            baseDir: './',
-        },
-    });
+gulp.task("browser-sync", () => {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
 
-// compile less files
-gulp.task('less', () => {
-    gulp.src('./assets/**/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('./assets'));
+gulp.task("less", () => {
+  gulp
+    .src("./assets/**/*.less")
+    .pipe(less())
+    .pipe(gulp.dest("./assets"));
 });
 
-// clean up css
-gulp.task('minify-css', () => {
-    gulp.src('./assets/**/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('./assets'));
+gulp.task("minify-css", () => {
+  gulp
+    .src("./assets/**/*.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./assets"));
 });
 
-// watch files for changes
-gulp.task('watch', () => {
-    gulp.watch('./assets/**/*.less', ['less']).on('change', bs.reload);
-    gulp.watch('./assets/**/*.css', ['minify-css']).on('change');
+gulp.task("watch", () => {
+  gulp.watch("./assets/**/*.less", ["less"]).on("change", bs.reload);
+  gulp.watch("./assets/**/*.css", ["minify-css"]).on("change");
 });
 
-// set default tasks
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task("default", ["browser-sync", "watch"]);
